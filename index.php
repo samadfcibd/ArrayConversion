9ce5edbbff0dcbd7b1c4eba28153fdfdd14568bf
Abdus Samad <samadocpl@gmail.com>
2019-12-28 16:00:56 +0600

    Composer init, psr-4 autoload added

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+require('DataTable.php');
+
+use DT_library\DataTable;
+
+// Multiple array
+$data = [
+    [
+        'name' => 'Abdus Samad',
+        'email' => 'samadocpl@gmail.com'
+    ],
+    [
+        'name' => 'Ibrahim Ahad',
+        'email' => 'ahad@gmail.com'
+    ]
+];
+
+// Object
+//$data = (object) $data;
+
+// Single array
+//$data = [
+//        'name' => 'Abdus Samad',
+//        'email' => 'samadocpl@gmail.com'
+//];
+
+
+if (isset($_POST['downloadCSV'])) {
+    $table = new DataTable($data);
+    $table->toCSV();
+}
+
+$toTableInitiate = new DataTable($data);
+$toTable = $toTableInitiate->toTable();
+
+
+$toTableInitiate = new DataTable($data);
+$toJson = $toTableInitiate->toJson();
+
+
+$toTableInitiate = new DataTable($data);
+$toXml = $toTableInitiate->toXml();
+
+$value = 'Edit';
+$sl = 0;
+$toTableInitiate = new DataTable($data);
+$table_after_addcolumn = $toTableInitiate->addColumn('#SL', function () use (&$sl) {
+    return ++$sl;
+})
+    ->addColumn('Action', function ($data) use ($value) {
+        return '<button class="btn btn-success btn-xs" title="' . $data['name'] . '">' . $value . '</button>';
+    })
+    ->toTable();
+
+
+$sl1 = 0;
+$toTableInitiate = new DataTable($data);
+$table_after_removeColumn = $toTableInitiate->addColumn('#SL', function () use (&$sl1) {
+    return ++$sl1;
+})
+    ->addColumn('Action', function ($data) use ($value) {
+        return '<button class="btn btn-success btn-xs" title="' . $data['name'] . '">' . $value . '</button>';
+    })
+    ->removeColumn('email')
+    ->toTable();
+
+
+$toTableInitiate = new DataTable($data);
+$table_with_editColumn = $toTableInitiate->editColumn('email', function ($data) {
+    return 'Email: ' . $data['email'];
+})->toTable();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container" id="main-content">
+    <h3>Data-<?php echo gettype($data); ?></h3>
+    <pre>
+        <?php print_r($data); ?>
+    </pre>
+
+    <h3>To Table</h3>
+    <?php print_r($toTable); ?>
+
+    <h3>To Json</h3>
+    <?php print_r($toJson); ?>
+
+    <h3>To XML</h3>
+    <textarea rows="10" cols="40">
+    <?php echo $toXml; ?>
+    </textarea>
+
+
+    <h3>To CSV</h3>
+    <form method="post">
+        <button class="btn btn-success" type="submit" value="downloadCSV" name="downloadCSV">Download CSV file</button>
+    </form>
+
+    <h3>Add Column</h3>
+    <?php print_r($table_after_addcolumn); ?>
+
+    <h3>Remove Column</h3>
+    <?php print_r($table_after_removeColumn); ?>
+
+    <h3>Edit Column</h3>
+    <?php print_r($table_with_editColumn); ?>
+
+</div>
+
+<script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+</body>
+</html>
+
+
---
+++ src/Classes/ArrayConversion.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace ArrayConversion\Classes;
+
+use SimpleXMLElement;
+
+class ArrayConversion
+{
+
+    /*
+     * 1 = Single array
+     * 2 = Multidimensional array
+     */
+    private $input_type;
+
+    private $data;
+
+    public function __construct($data)
+    {
+        // if the data is object type then convert it into array
+        if (is_object($data)) {
+            $data = (array)$data;
+        }
+
+        // Check data type
+        if (!is_array($data)) {
+            echo 'Data type should be array or object';
+            exit();
+        }
+
+        if (count($data) != count($data, COUNT_RECURSIVE)) {
+            $this->input_type = 2;
+            $this->data = $data;
+        } else {
+            $this->input_type = 1;
+            $this->data = $data;
+        }
+    }
+
+    public function toTable()
+    {
+        if ($this->input_type === 2) {
+
+            $table_head_columns = '<thead><tr>';
+            foreach (array_keys($this->data[0]) as $key => $value) {
+                $table_head_columns .= '<th>' . $value . '</th>';
+            }
+            $table_head_columns .= '</tr><thead>';
+
+            $table_body_columns = '';
+            foreach ($this->data as $key => $value) {
+                $table_body_columns .= '<tr>';
+                foreach ($value as $key => $value) {
+                    $table_body_columns .= '<td>' . $value . '</td>';
+                }
+                $table_body_columns .= '</tr>';
+            }
+            $table_body_columns = '<tbody>' . $table_body_columns . '</tbody>';
+            return '<table class="table table-bordered">' . $table_head_columns . $table_body_columns . '</table>';
+
+        } else {
+
+            $table_head_columns = '<thead><tr>';
+            $table_body_columns = '<tbody><tr>';
+            foreach ($this->data as $key => $value) {
+                $table_head_columns .= '<th>' . $key . '</th>';
+                $table_body_columns .= '<td>' . $value . '</td>';
+            }
+            $table_head_columns .= '<tr></thead>';
+            $table_body_columns .= '<tr></tbody>';
+            return '<table class="table table-bordered">' . $table_head_columns . $table_body_columns . '</table>';
+        }
+    }
+
+    public function addColumn($column_name, $user_function)
+    {
+        if ($this->input_type === 1) {
+            //$this->data[$column_name] = $user_function($this->data);
+            $this->data[$column_name] = call_user_func($user_function, $this->data);
+        } else {
+            foreach ($this->data as $key => $row) {
+                //$this->data[$key][$column_name] = $user_function->__invoke($row);
+                $this->data[$key][$column_name] = call_user_func($user_function, $row);
+            }
+        }
+        return $this;
+    }
+
+    public function removeColumn()
+    {
+        $arguments = func_get_args();
+
+        if (!empty($arguments)) {
+            if ($this->input_type === 1) {
+                foreach ($arguments as $column) {
+                    unset($this->data[$column]);
+                }
+            } else {
+                foreach ($this->data as $key => $row) {
+                    foreach ($arguments as $column) {
+                        //unset($row[$column]);
+                        unset($this->data[$key][$column]);
+                    }
+                }
+            }
+        }
+
+//        echo '<pre>';
+//        print_r($arguments);
+//        exit;
+        return $this;
+    }
+
+    public function editColumn($column_name, $user_function)
+    {
+        if ($this->input_type === 1) {
+            $this->data[$column_name] = $user_function($this->data);
+        } else {
+            foreach ($this->data as $key => $row) {
+                $this->data[$key][$column_name] = $user_function($row);
+            }
+        }
+
+        return $this;
+    }
+
+    public function toJson()
+    {
+        return json_encode($this->data);
+    }
+
+    public function toXml()
+    {
+        // Creating a object of simple XML element
+        $xml = new SimpleXMLElement('<?xml version="1.0"?><dataTable></dataTable>');
+
+        // Visit all key value pair
+        foreach ($this->data as $k => $v) {
+
+            // If there is nested array then
+            if (is_array($v)) {
+                $child = $xml->addChild("row_$k");
+                foreach ($v as $key => $value) {
+                    $child->addChild($key, $value);
+                }
+            } else {
+                $xml->addChild($k, $v);
+            }
+        }
+        return $xml->saveXML();
+    }
+
+    public function toCSV()
+    {
+
+        $fileName = 'ArrayConversion.csv';
+
+        header("Content-Type: application/csv; charset=UTF-8");
+        header("Content-Disposition: attachment; filename={$fileName}");
+
+        // Open file with write mode
+        $f = fopen('php://output', 'w');
+
+        // Set first row as header
+        $set_column_name = false;
+        if ($this->input_type === 1) {
+            fputcsv($f, array_keys($this->data));
+            $set_column_name = true;
+        }
+
+        foreach ($this->data as $row) {
+            // Set first row as header, if it hasn't been added yet
+            if (!$set_column_name) {
+                fputcsv($f, array_keys($row));
+                $set_column_name = true;
+            }
+
+            fputcsv($f, $row);
+        }
+        // Close the file
+        fclose($f);
+        // Make sure nothing else is sent, our file is done
+        exit;
+    }
+}
