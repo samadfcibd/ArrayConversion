c67a8211e51674346b828f88a1979dfb5cb2e9d9
Abdus Samad <samadocpl@gmail.com>
2019-12-31 16:42:11 +0600

    PHP_CodeSniffer standard following

--- src/Classes/ArrayConversion.php
+++ src/Classes/ArrayConversion.php
@@ -1,22 +1,78 @@
 <?php
 
-namespace ArrayConversion\Classes;
+/**
+ * ArrayConversion class used for conversion given array.
+ * PHP Version 5
+ *
+ * @param array | object
+ *
+ * @category Php-package
+ *
+ * @package ArrayConversion
+ *
+ * @author Abdus Samad <samadocpl@gmail.com>
+ *
+ * @copyright 2019-2020 Abdus Samad
+ *
+ * @license https://opensource.org/licenses/MIT MIT
+ *
+ * @link https://github.com/samadfcibd/ArrayConversion
+ *
+ * @note This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
+namespace ArrayConversion\Classes;
 
 use SimpleXMLElement;
 use InvalidArgumentException;
 
+/**
+ * ArrayConversion class used for conversion given array.
+ *
+ * At first input data will assigned to $data,
+ * after that this data will be processed as per as user demand.
+ *
+ * @param array | object
+ *
+ * @category Php-package
+ *
+ * @package ArrayConversion
+ *
+ * @author Abdus Samad <samadocpl@gmail.com>
+ *
+ * @license https://opensource.org/licenses/MIT MIT
+ *
+ * @Release: <package_version> 1.0.0
+ *
+ * @link https://github.com/samadfcibd/ArrayConversion
+ *
+ * @since 011219
+ */
 class ArrayConversion
 {
 
-    /*
-     * 1 = Single array
+    /**
+     * Input data type
+     * 1 = single array
      * 2 = Multidimensional array
+     *
+     * @var array
      */
-    private $input_type;
+    private $_input_type;
 
-    private $data;
+    /**
+     * Input data
+     *
+     * @var array
+     */
+    private $_data;
 
+    /**
+     * ArrayConversion constructor.
+     * @param $data
+     */
     public function __construct($data)
     {
         // if the data is object type then convert it into array
@@ -31,26 +87,26 @@ class ArrayConversion
         }
 
         if (count($data) != count($data, COUNT_RECURSIVE)) {
-            $this->input_type = 2;
-            $this->data = $data;
+            $this->_input_type = 2;
+            $this->_data = $data;
         } else {
-            $this->input_type = 1;
-            $this->data = $data;
+            $this->_input_type = 1;
+            $this->_data = $data;
         }
     }
 
     public function toTable()
     {
-        if ($this->input_type === 2) {
+        if ($this->_input_type === 2) {
 
             $table_head_columns = '<thead><tr>';
-            foreach (array_keys($this->data[0]) as $key => $value) {
+            foreach (array_keys($this->_data[0]) as $key => $value) {
                 $table_head_columns .= '<th>' . $value . '</th>';
             }
             $table_head_columns .= '</tr><thead>';
 
             $table_body_columns = '';
-            foreach ($this->data as $key => $value) {
+            foreach ($this->_data as $key => $value) {
                 $table_body_columns .= '<tr>';
                 foreach ($value as $key => $value) {
                     $table_body_columns .= '<td>' . $value . '</td>';
@@ -64,7 +120,7 @@ class ArrayConversion
 
             $table_head_columns = '<thead><tr>';
             $table_body_columns = '<tbody><tr>';
-            foreach ($this->data as $key => $value) {
+            foreach ($this->_data as $key => $value) {
                 $table_head_columns .= '<th>' . $key . '</th>';
                 $table_body_columns .= '<td>' . $value . '</td>';
             }
@@ -76,13 +132,13 @@ class ArrayConversion
 
     public function addColumn($column_name, $user_function)
     {
-        if ($this->input_type === 1) {
-            //$this->data[$column_name] = $user_function($this->data);
-            $this->data[$column_name] = call_user_func($user_function, $this->data);
+        if ($this->_input_type === 1) {
+            //$this->_data[$column_name] = $user_function($this->_data);
+            $this->_data[$column_name] = call_user_func($user_function, $this->_data);
         } else {
-            foreach ($this->data as $key => $row) {
-                //$this->data[$key][$column_name] = $user_function->__invoke($row);
-                $this->data[$key][$column_name] = call_user_func($user_function, $row);
+            foreach ($this->_data as $key => $row) {
+                //$this->_data[$key][$column_name] = $user_function->__invoke($row);
+                $this->_data[$key][$column_name] = call_user_func($user_function, $row);
             }
         }
         return $this;
@@ -93,15 +149,15 @@ class ArrayConversion
         $arguments = func_get_args();
 
         if (!empty($arguments)) {
-            if ($this->input_type === 1) {
+            if ($this->_input_type === 1) {
                 foreach ($arguments as $column) {
-                    unset($this->data[$column]);
+                    unset($this->_data[$column]);
                 }
             } else {
-                foreach ($this->data as $key => $row) {
+                foreach ($this->_data as $key => $row) {
                     foreach ($arguments as $column) {
                         //unset($row[$column]);
-                        unset($this->data[$key][$column]);
+                        unset($this->_data[$key][$column]);
                     }
                 }
             }
@@ -115,11 +171,11 @@ class ArrayConversion
 
     public function editColumn($column_name, $user_function)
     {
-        if ($this->input_type === 1) {
-            $this->data[$column_name] = $user_function($this->data);
+        if ($this->_input_type === 1) {
+            $this->_data[$column_name] = $user_function($this->_data);
         } else {
-            foreach ($this->data as $key => $row) {
-                $this->data[$key][$column_name] = $user_function($row);
+            foreach ($this->_data as $key => $row) {
+                $this->_data[$key][$column_name] = $user_function($row);
             }
         }
 
@@ -128,7 +184,7 @@ class ArrayConversion
 
     public function toJson()
     {
-        return json_encode($this->data);
+        return json_encode($this->_data);
     }
 
     public function toXml()
@@ -137,7 +193,7 @@ class ArrayConversion
         $xml = new SimpleXMLElement('<?xml version="1.0"?><dataTable></dataTable>');
 
         // Visit all key value pair
-        foreach ($this->data as $k => $v) {
+        foreach ($this->_data as $k => $v) {
 
             // If there is nested array then
             if (is_array($v)) {
@@ -165,12 +221,12 @@ class ArrayConversion
 
         // Set first row as header
         $set_column_name = false;
-        if ($this->input_type === 1) {
-            fputcsv($f, array_keys($this->data));
+        if ($this->_input_type === 1) {
+            fputcsv($f, array_keys($this->_data));
             $set_column_name = true;
         }
 
-        foreach ($this->data as $row) {
+        foreach ($this->_data as $row) {
             // Set first row as header, if it hasn't been added yet
             if (!$set_column_name) {
                 fputcsv($f, array_keys($row));
