0d87ab3951b7e5a2c465ec1bd8df72ffa893b4eb
Abdus Samad <samadocpl@gmail.com>
2020-02-16 16:50:54 +0600

    Webhook tested

--- src/Contracts/OutputInterface.php
+++ src/Contracts/OutputInterface.php
@@ -2,6 +2,12 @@
 
 namespace ArrayConversion\Contracts;
 
+/**
+ * Interface OutputInterface
+ * @package ArrayConversion\Contracts
+ *
+ * Output interface
+ */
 interface OutputInterface
 {
     public function getOutput($array_type, $array);
